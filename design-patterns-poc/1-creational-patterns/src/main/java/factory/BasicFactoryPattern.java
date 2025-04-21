9aa2c1e723ff6ab91bcbd0b3b265dca0cc68d464
girish.balekundri <girish.thetechieguy@gmail.com>
2025-04-21 23:58:04 +0800

    Factory pattern basic version is explained!

---
+++ design-patterns-poc/1-creational-patterns/src/main/java/factory/BasicFactoryPattern.java
@@ -0,0 +1,62 @@
+package factory;
+
+interface Shape{
+    void draw();
+}
+
+class Rectangle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Rectangle Draw () is called!!");
+    }
+}
+
+class Square implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Square Draw() is called!!");
+    }
+}
+
+class Circle implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Circle Draw() is called!!");
+    }
+}
+
+
+class ShapeFactory {
+    public Shape getShape(String shape){
+        if(shape ==  null){
+            return null;
+        } else if (shape.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shape.equalsIgnoreCase("RECTANGLE")){
+            return new Rectangle();
+        } else if (shape.equalsIgnoreCase("SQUERE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
+
+public class BasicFactoryPattern{
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+
+        //get an object of Circle and call its draw method.
+        Shape shape1 = shapeFactory.getShape("CIRCLE");
+        shape1.draw();
+
+        //get an object of Rectangle and call its draw method.
+        Shape shape2 = shapeFactory.getShape("RECTANGLE");
+        shape2.draw();
+
+        //get an object of Square and call its draw method.
+        Shape shape3 = shapeFactory.getShape("SQUARE");
+        shape3.draw();
+    }
+}
